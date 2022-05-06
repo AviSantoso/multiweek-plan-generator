70cfc4f1d6a8f23a3cea88194d52476f39b6c69d
Aviciena Santoso <avi.santoso@gmail.com>
2022-05-06 14:38:03 +0800

    Generate empty document

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,148 @@
-import * as React from "react"
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
+  Center,
   Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Button,
+  GridItem,
+  Divider,
+  Heading,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import moment from "moment";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  usePDF,
+} from "@react-pdf/renderer";
+import FileSaver from "file-saver";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+import { AviSelect } from "./components/AviSelect";
+
+const divisionOptions = [
+  { value: "1", text: "Week" },
+  { value: "2", text: "Fortnight" },
+  { value: "4", text: "Four Weeks" },
+];
+
+const firstOptions = [
+  { value: "mon", text: "Monday" },
+  { value: "sun", text: "Sunday" },
+];
+
+const countOptions = Array.from(new Array(20)).map((_, i) => {
+  const item = `${i + 1}`;
+  return { value: item, text: item };
+});
+
+const startingOptions = (() => {
+  const options = [];
+  let time = moment().day(0);
+  for (let i = 0; i < 8; i++) {
+    const value = "" + time.utc();
+    const text = time.format("YYYY / MM / DD");
+    options.push({ value, text });
+    time.add(1, "w");
+  }
+  return options;
+})();
+
+function MyDocument() {
+  // Create styles
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: "row",
+      backgroundColor: "#E4E4E4",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+  });
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>Section #1</Text>
+        </View>
+        <View style={styles.section}>
+          <Text>Section #2</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+}
+
+export function App() {
+  const [division, setDivision] = useState("1");
+  const [count, setCount] = useState("1");
+  const [first, setFirst] = useState("mon");
+  const [starting, setStarting] = useState("");
+
+  const [instance, updateInstance] = usePDF({ document: <MyDocument /> });
+
+  if (instance.loading) return <div>Loading ...</div>;
+  if (instance.error) return <div>Something went wrong: {instance.error}</div>;
+
+  async function onGeneratePDF() {
+    if (instance.blob) {
+      FileSaver(instance.blob, "planner.pdf");
+    }
+  }
+
+  return (
+    <Center w="100vw" h="100vh" p={4} overflow="auto" bg="orange.100">
+      <Grid p={4} gap={4} borderRadius={4} templateColumns="1fr 3fr" bg="white">
+        <GridItem colSpan={2}>
+          <Heading size="md" fontWeight={400}>
+            Multi Week Plan Generator
+          </Heading>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Divider />
+        </GridItem>
+        <AviSelect
+          label="Division Size"
+          options={divisionOptions}
+          value={division}
+          onValue={setDivision}
+        />
+        <AviSelect
+          label="Division Count"
+          options={countOptions}
+          value={count}
+          onValue={setCount}
+        />
+        <AviSelect
+          label="First Day"
+          options={firstOptions}
+          value={first}
+          onValue={setFirst}
+        />
+        <AviSelect
+          label="Starting From"
+          options={startingOptions}
+          value={starting}
+          onValue={setStarting}
+        />
+        <GridItem colSpan={2}>
+          <Divider />
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Button
+            variant="outline"
+            colorScheme="orange"
+            w="full"
+            onClick={onGeneratePDF}
           >
-            Learn Chakra
-          </Link>
-        </VStack>
+            Generate PDF
+          </Button>
+        </GridItem>
+        {/* <GridItem colSpan={2} h="0" bg="gray" paddingBottom="140%"></GridItem> */}
       </Grid>
-    </Box>
-  </ChakraProvider>
-)
+    </Center>
+  );
+}
---
+++ src/components/AviSelect.tsx
@@ -0,0 +1,40 @@
+import { Center, GridItem, FormLabel, Select } from "@chakra-ui/react";
+import { useMemo } from "react";
+import slugify from "slugify";
+
+export interface SelectOption {
+  value: string;
+  text: string;
+}
+
+export interface AviSelectProps {
+  label: string;
+  value: string;
+  onValue: (value: string) => void;
+  options: SelectOption[];
+}
+
+export function AviSelect({ label, value, onValue, options }: AviSelectProps) {
+  const name = useMemo(() => slugify(`AviSelect ${label}`), [label]);
+  return (
+    <>
+      <Center as={GridItem} justifyContent="left">
+        <FormLabel htmlFor={name} fontWeight={400}>
+          {label}
+        </FormLabel>
+      </Center>
+      <Center as={GridItem}>
+        <Select
+          id={name}
+          value={value}
+          onChange={(e) => onValue(e.target.value)}
+          placeholder="Select option"
+        >
+          {options.map((option) => (
+            <option value={option.value}>{option.text}</option>
+          ))}
+        </Select>
+      </Center>
+    </>
+  );
+}
